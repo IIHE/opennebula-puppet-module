b0682ddfa676b42120249e3c47879a49a1222b0b
Stéphane Gerard <stgerard@vub.ac.be>
2022-03-24 11:30:08 +0100

    adding to compute_node the possibility to have libvirt accessing a ceph rdb

--- manifests/compute_node.pp
+++ manifests/compute_node.pp
@@ -26,11 +26,14 @@
 # http://www.apache.org/licenses/LICENSE-2.0.html
 #
 class one::compute_node(
-  $puppetdb = $one::puppetdb,
-  $oneid    = $one::oneid,
-  $im_mad   = $one::im_mad,
-  $vm_mad   = $one::vm_mad,
-  $vn_mad   = $one::vn_mad,
+  $puppetdb          = $one::puppetdb,
+  $oneid             = $one::oneid,
+  $im_mad            = $one::im_mad,
+  $vm_mad            = $one::vm_mad,
+  $vn_mad            = $one::vn_mad,
+  $libvirt_with_ceph = false,
+  $libvirt_rbd_secret_uuid = undef,
+  $libvirt_rbd_secret_key  = undef,
 ) {
   include one::prerequisites
   include one::install
@@ -47,6 +50,11 @@ class one::compute_node(
   Class['one::compute_node::config'] ~>
   Class['one::compute_node::service'] ~>
   Class['one::service']
+
+  if ( $libvirt_with_ceph ) {
+    include one::compute_node::libvirt_with_ceph
+    Class['one::compute_node::service'] -> Class['one::compute_node::libvirt_with_ceph']
+  }
 
   if ($puppetdb == true) {
     # Register the node as a onehost in the puppetdb
---
+++ manifests/compute_node/libvirt_with_ceph.pp
@@ -0,0 +1,53 @@
+# to configure libvirt so that it can use a ceph rbd pool
+# source: https://github.com/openstack/puppet-nova/blob/master/manifests/compute/rbd.pp
+class one::compute_node::libvirt_with_ceph (
+  $libvirt_rbd_secret_uuid = hiera('one::compute_node::libvirt_rbd_secret_uuid', undef),
+  $libvirt_rbd_secret_key  = hiera('one::compute_node::libvirt_rbd_secret_key', undef),
+) {
+
+  if ( ($libvirt_rbd_secret_uuid != undef) and ($libvirt_rbd_secret_key != undef) ) {
+
+    $secret_dir = '/etc/libvirt/ceph';
+    $secret_xml_path = "${secret_dir}/secret.xml";
+    $virsh_secret_path = "${secret_dir}/virsh.secret";
+    file { $secret_dir:
+      ensure => directory,
+      mode   => '0640',
+      owner  => 'root',
+      group  => 'root',
+    }
+    file { $secret_xml_path:
+      content => template('profile/secret.xml.erb'),
+      mode   => '0640',
+      owner  => 'root',
+      group  => 'root',
+      require => File[$secret_dir],
+    }
+    file { $virsh_secret_path:
+      mode   => '0640',
+      owner  => 'root',
+      group  => 'root',
+      require => File[$secret_dir],
+    }
+
+    # define the virsh secret
+    $cm = "/usr/bin/virsh secret-define --file $secret_xml_path | /usr/bin/awk \'{print $2}\' | sed \'/^$/d\' > $virsh_secret_path"
+    exec { 'get-or-set virsh secret':
+      command => $cm,
+      unless  => "/usr/bin/virsh secret-list | grep -i ${libvirt_rbd_secret_uuid}",
+      require => File[$secret_xml_path],
+    }
+
+    # set the value of virsh secret
+    exec { 'set-secret-value virsh':
+      command => "/usr/bin/virsh secret-set-value --secret ${libvirt_rbd_secret_uuid} --base64 ${libvirt_rbd_secret_key}",
+      unless  => "/usr/bin/virsh secret-get-value ${libvirt_rbd_secret_uuid} | grep ${libvirt_rbd_secret_key}",
+      require => Exec['get-or-set virsh secret'],
+    }
+
+  } else {
+
+    fail('When configuring libvirt for ceph, you must define the rbd secret uuid and key.')
+  }
+
+}
