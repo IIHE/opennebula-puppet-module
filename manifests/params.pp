8d53b737442d8b12a9984af749b105e1576c6d9a
Stéphane Gerard <stgerard@vub.ac.be>
2022-04-15 16:39:05 +0200

    debugging

--- manifests/params.pp
+++ manifests/params.pp
@@ -98,7 +98,7 @@ class one::params {
   $vnc_proxy_cert            = hiera('one::oned::vnc_proxy_cert', '')
   $vnc_proxy_key             = hiera('one::oned::vnc_proxy_key', '')
   $vnc_proxy_ipv6            = hiera('one::oned::vnc_proxy_ipv6', 'false') # lint:ignore:quoted_booleans
-  $sunstone_fireedge         = hiera('one::sunstone_fireedge', 'false')
+  $sunstone_fireedge         = $one::sunstone_fireedge
 
   # generic params for nodes and oned
   $oneuid = '9869'
